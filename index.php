191ff72ec2f8b142f62dfcf963725e22641873ee
Clement <clement.esnault@etu.unicaen.fr>
2025-12-10 16:34:16 +0100

    donne

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (session_status() === PHP_SESSION_NONE) {
             <p id="titre">Only Stage</p>
         </div>
         
-         <?php 
+        <?php 
         include_once 'src/php/Navbar.php' ;
         ?>
 
