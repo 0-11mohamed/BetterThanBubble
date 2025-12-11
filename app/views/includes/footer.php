069e96ee203e196aed2a18cd751a8459aded9ada
woipy feur apagnan <etupass@etu.unicaen.fr>
2025-12-11 08:38:17 +0100

    Fix ?

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -1,5 +1,5 @@
         </main>
         <footer >
             <h1>FOOTER</h1>
-            <p>&copy; 2025 DungeonXplorer. Tous droits réservés.</p>
+            <p>&copy; 2025 OnlyStage. Tous droits réservés.</p>
         </footer>
